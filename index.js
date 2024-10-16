0783caf4ead88e3b0fee7bcf86f6418bda0c21a2
johntiger501 <johnragthor@gmail.com>
2024-10-16 10:25:16 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
